1c58ac8fb7f5f6ca4354a7acf55148f306b2d3ae
ChitreshN <chitreshnarra@gmail.com>
2024-07-10 22:18:53 +0530

    feat: add terminal arguments to the program

--- main.go
+++ main.go
@@ -2,19 +2,36 @@ package main
 
 import (
 	"fmt"
+	"github.com/got/lib"
 	"os"
 )
 
-func main (){
-    argLength := len(os.Args)
-    if argLength < 2 {
-        fmt.Println("usage: ./got command [options]")
-    }
-    command := os.Args[1]
-    switch command {
-    case "init":
-        Init()
-    case "add":
-        Add(os.Args[2])
-    }
+func main() {
+	argLength := len(os.Args)
+	if argLength < 2 {
+		fmt.Println("usage: ./got command [options]")
+		os.Exit(1)
+	}
+	command := os.Args[1]
+	switch command {
+	case "init":
+		lib.Init()
+	case "add":
+		lib.Add(os.Args[2])
+	case "status":
+        tracked,untracked := lib.Status()
+        println("The following lines are tracked:")
+        for _, fileName := range tracked {
+           println(fileName) 
+        }
+        println()
+        println("The following lines are untracked:")
+        for _, fileName := range untracked {
+           println(fileName) 
+        }
+    case "diff":
+        if argLength < 3 {
+            fmt.Println("usage: ./got diff file1")
+        }
+	}
 }
