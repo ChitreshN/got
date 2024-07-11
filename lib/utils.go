987018cb189591fab691bec9c61343ef50320e51
ChitreshN <chitreshnarra@gmail.com>
2024-07-11 22:17:01 +0530

    style: move execution to utils

--- lib/utils.go
+++ lib/utils.go
@@ -1,7 +1,47 @@
 package lib
 
+import (
+	"fmt"
+	"os"
+)
+
 func Check(e error) {
-    if e != nil{
-        panic(e)
-    }
+	if e != nil {
+		fmt.Printf("error: %v", e)
+		os.Exit(1)
+	}
+}
+
+func RunStatus() {
+	tracked, untracked := Status()
+	fmt.Printf("The following files are tracked:\n")
+	for _, fileName := range tracked {
+		fmt.Printf("\033[32m%s\033[0m\n", fileName)
+	}
+	fmt.Printf("The following files are untracked:\n")
+	for _, fileName := range untracked {
+		fmt.Printf("\033[31m%s\033[0m\n", fileName)
+	}
+}
+
+func RunDiff() {
+	file1, err := os.OpenFile(os.Args[2], os.O_RDONLY, 0666)
+	Check(err)
+	// how handle first commit?
+	objFile := ".got/obj/" + os.Args[2]
+	file2, err := os.OpenFile(objFile, os.O_RDONLY, 0666)
+	if err != nil {
+		fmt.Println("commit first before diffing it")
+	}
+	diff := Diff(file1, file2)
+	for _, val := range diff {
+		switch val.EditType {
+		case Append:
+			fmt.Printf("\033[32m+%s\033[0m\n", val.Append)
+		case Delete:
+			fmt.Printf("\033[31m-%s\033[0m\n", val.Delete)
+		case Identical:
+			fmt.Printf("%s\n", val.Identical)
+		}
+	}
 }
